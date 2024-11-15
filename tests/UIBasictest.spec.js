bdc6eddce3aa6470d310157619bed2ff809d3782
nro <nro@nro.nro>
2024-11-15 17:07:27 -0500

    added UIBasictest

---
+++ tests/UIBasictest.spec.js
@@ -0,0 +1,15 @@
+const { test, expect } = require('@playwright/test');
+
+// Set browser context
+test.only('Browser Context Playwright test', async ({ browser }) => {
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    await page.goto('https://rahulshettyacademy.com/loginpagePractise/');
+    await expect(page).toHaveTitle(/Login/);
+    console.log(await page.title());
+    //css, xpath
+    await page.locator('#username').fill('rahulshetty');
+    await page.locator('#password').fill('learning');
+    await page.locator('#signInBtn').click();
+    console.log(await page.locator(`div[style*="block"]`).textContent("Incorrect"));
+});
